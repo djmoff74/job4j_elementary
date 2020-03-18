d3207b25c63aadf311b9315dcdd73c167d7faf9a
ELykov <djmoff74@gmail.com>
2020-03-18 11:46:13 +0500

    9.1. String [#237407]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+import javax.crypto.spec.PSource;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java! ";
+        System.out.println(idea);
+        int year = 2020;
+        idea = idea + "But I am newbie. " + year;
+        System.out.println(idea);
+
+    }
+}
