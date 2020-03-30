1ae32ba0cdff934bc22ca67b0311582fbbf55299
djmoff74 <djmoff74@gmail.com>
2020-03-30 11:32:44 +0500

    6.0.1. Объявление массива. [#3613]

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+/**
+ * Class ArrayDefinition
+ *
+ * @author Eduard Lykov (djmoff74@gmail.com)
+ * @version 1.0
+ * @since 30.03.2020
+ */
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}
